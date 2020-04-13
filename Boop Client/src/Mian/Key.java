3098147cf6c1e3d9b473a06b480f6cdcd36a4708
NonIntelligent <amaanmiah10@gmail.com>
2020-04-13 18:53:16 +0100

    Started working on keyboard and mouse input classes

---
+++ Boop Client/src/Mian/Key.java
@@ -0,0 +1,15 @@
+package Mian;
+
+import java.awt.event.KeyEvent;
+
+public enum Key {
+	
+	W(KeyEvent.VK_W, 0);
+	
+	int keyCode, methodIndex;
+	
+	private Key(int keyCode, int methodIndex) throws ExceptionInInitializerError{
+		this.keyCode = keyCode;
+		this.methodIndex = methodIndex;
+	}
+}
