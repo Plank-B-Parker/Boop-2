b2e41d2114faa02357b52fda1eacb6657f41652e
PlankASS <meeharbi_noiz@outlook.com>
2020-05-11 20:44:43 +0100

    Rendering in storage in server done

--- Boop Server/src/Balls/Storage.java
+++ Boop Server/src/Balls/Storage.java
@@ -32,16 +32,33 @@ public class Storage {
 			float x = pos.x + vel.x*dt;
 			float y = pos.y + vel.y*dt;
 			
+			//Scaling for screen.
 			int X = (int)((x + 1)*0.5*main.windowWidth);
 			int Y = (int)((y + 1)*0.5*main.windowHeight);
 			int RadX = (int)(0.5*rad*main.windowWidth);
 			int RadY = (int)(0.5*rad*main.windowHeight);
 			
+			//Second set of coordinates for edge.
+			int X2 = X;
+			int Y2 = Y;
+			
 			if(x + rad > 1 && x - rad < 1) {
-				
+				X2 = X - main.windowWidth;
 			}
 			else if(x + rad > -1 && x - rad < -1) {
-				
+				X2 = X - main.windowWidth;
+			}
+			
+			if(y + rad > 1 && y - rad < 1) {
+				Y2 = Y - main.windowHeight;
+			}
+			else if(y + rad > -1 && y - rad < -1) {
+				Y2 = Y - main.windowHeight;
+			}
+			
+			g.drawOval(X, Y, RadX, RadY);
+			if(X2 != X && Y2 != Y) {
+				g.drawOval(X2, Y2, RadX, RadY);
 			}
 			
 		}
