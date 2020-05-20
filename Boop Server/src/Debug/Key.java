c8a59a41f141f009d412e149cd5859c458d2501b
Amabuzi <dev.amabuzi@gmail.com>
2020-05-20 13:49:25 +0100

    Modularised and generalised keyboard functions

--- Boop Server/src/Debug/Key.java
+++ Boop Server/src/Debug/Key.java
@@ -3,20 +3,22 @@ package Debug;
 import java.awt.event.KeyEvent;
 
 public enum Key {
-	SPACE("Debug Grid", KeyEvent.VK_SPACE, false, -1),
-	K("Paused", KeyEvent.VK_K, false, -1),
-	L("Step-Forward", KeyEvent.VK_L, false, -1);
+	SPACE("Debug Grid", KeyEvent.VK_SPACE, false, true, -1),
+	K("Paused", KeyEvent.VK_K, false, true, -1),
+	L("Step-Forward", KeyEvent.VK_L, false, false, -1);
 	
 	
 	private String name;
 	private int keyCode;
 	private boolean hold;
+	private boolean toggle;
 	private long delay;
 	
-	private Key(String name, int keyCode, boolean hold, long delay){
+	private Key(String name, int keyCode, boolean hold, boolean toggle, long delay){
 		this.name = name;
 		this.keyCode = keyCode;
 		this.hold = hold;
+		this.toggle = toggle;
 		this.delay = delay;
 	}
 
@@ -30,6 +32,10 @@ public enum Key {
 
 	public boolean isHold() {
 		return hold;
+	}
+
+	public boolean isToggle() {
+		return toggle;
 	}
 
 	public long getDelay() {
